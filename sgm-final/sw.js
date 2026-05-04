b7df96bad9a2f8318c390eb50d87ab4b7866b49a
mmovalentin <mmovalentin@gmail.com>
2026-05-04 15:09:38 -0300

    Delete sgm-final directory

--- sgm-final/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='sgm-v1';
-const ASSETS=['/','/index.html','/manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('/api/'))return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)).catch(()=>caches.match('/index.html')));});
